5f0a647f0f7f3f2d67efee66da955c893346530b
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-06-10 07:31:42 +0000

    game board is created

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // UI: game board
-// gameboard needs to be stored inside array
+// const createBoard = (() => {
+//   const board = document.querySelector(".board");
+//   for (let i = 0; i < 9; i++) {
+//     const squares = document.createElement("div");
+//     squares.classList.add("square");
+//     board.appendChild(squares);
+//   }
+// })();
+
 const gameBoard = (() => {
-  const board = document.querySelector(".board");
-  for (let i = 0; i < 9; i++) {
-    const squares = document.createElement("div");
-    squares.classList.add("square");
-    board.appendChild(squares);
-    console.log(board);
-  }
+  const boardDiv = document.querySelector(".board");
+  let boardData = {};
+  const createBoardObj = (() => {
+    const squares = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
+    squares.forEach((item) => {
+      boardData[item] = "";
+      const squaresDiv = document.createElement("div");
+      squaresDiv.classList.add("square");
+      boardDiv.appendChild(squaresDiv);
+    });
+  })();
+  const getBoard = () => {
+    return boardData;
+  };
+
+  return { getBoard };
 })();
+
+const createPlayer = () => {
+  console.log("uwu");
+};
