bcb21834a604951966619af3cce0ede6becfc682
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-06-10 10:54:06 +0000

    working on makeMove feature

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
-// UI: game board
-// const createBoard = (() => {
-//   const board = document.querySelector(".board");
-//   for (let i = 0; i < 9; i++) {
-//     const squares = document.createElement("div");
-//     squares.classList.add("square");
-//     board.appendChild(squares);
-//   }
-// })();
-
+// game board
+// draws the UI of game board
 const gameBoard = (() => {
   const boardDiv = document.querySelector(".board");
-  let boardData = {};
-  const createBoardObj = (() => {
-    const squares = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
-    squares.forEach((item) => {
-      boardData[item] = "";
+  let boardData = [];
+  const createBoard = () => {
+    for (let i = 0; i < 9; i++) {
       const squaresDiv = document.createElement("div");
       squaresDiv.classList.add("square");
       boardDiv.appendChild(squaresDiv);
-    });
-  })();
+      boardData.push("empty");
+    }
+  };
   const getBoard = () => {
     return boardData;
   };
+  const placeMark = (position, mark) => {
+    // this function places player mark in desired position
+    boardData[position] = mark;
+    return boardData;
+  };
 
-  return { getBoard };
+  return { getBoard, createBoard, placeMark };
 })();
 
-const createPlayer = () => {
-  console.log("uwu");
+// player manager
+// just create player objects
+const createPlayer = (name, mark) => {
+  const playerName = name;
+  const playerMark = mark;
+  return { playerName, playerMark };
 };
+
+// game controller
+// traditionally player X goes first;
+const gameController = (() => {
+  const playerX = createPlayer("Ajay", "X"); //prompt("Choose player name for X")
+  const playerO = createPlayer("Vijay", "O");
+  let currentPlayer;
+  //Write code for turn
+  const playerTurn = () => {
+    // this function should return either playerX or playerO
+    currentPlayer = playerX; //first player
+    return currentPlayer;
+  };
+  const makeMove = (position) => {
+    // code to place mark via gameBoard.placeMark()
+    // position can be derived via eventlistner
+    gameBoard.placeMark(position, currentPlayer.playerMark);
+  };
+  // player will populate gameBoard.getBoard() with their mark;
+  return { playerTurn, makeMove };
+})();
+
+gameBoard.createBoard();
+gameController.playerTurn();
+console.log(gameController.playerTurn());
+console.log(gameController.makeMove(9));
